1e8bc3dee330d906d954ccafaf6fe03de898536b
niclew17 <niclewis17@gmail.com>
2024-10-17 13:46:48 -0600

    Added more to .yml

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,69 +1,76 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 class Calculator {
 
-    Calculator(){
+    private Set<String> uniqueIDs; // To keep track of unique IDs
 
+    Calculator() {
+        uniqueIDs = new HashSet<>();
     }
+
     // This will add values
-    int add(int a , int b){
+    int add(int a, int b) {
         return a + b;
     }
 
-    int subtract(int a , int b){
+    // This will subtract values
+    int subtract(int a, int b) {
         return a - b;
     }
 
-    int multiply(int a , int b){
+    // This will multiply values
+    int multiply(int a, int b) {
         return a * b;
     }
 
-    int divide(int a , int b){
+    // This will divide values
+    int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero.");
+        }
         return a / b;
     }
 
-
     /*
-    Returns the n'th number in the fibonacci sequence
-    https://en.wikipedia.org/wiki/Fibonacci_number
-    Example below
-    n = x
-    1 = 1
-    2 = 1
-    3 = 2
-    4 = 3
-    5 = 5
-    .
-    .
-    .
-    etc
+     Returns the n'th number in the Fibonacci sequence
      */
-    int fibonacciNumberFinder(int n){
-        return 0;
-    }
-
-
-    /*
-    Returns binary value of the given int number
-    The binary number will be stored as a string
-    if int a = 0 then this method returns: 0
-    if int a = 10 then this method returns: 1010
-    if int a = 16 then this method returns: 10000
-     */
-    String intToBinaryNumber(int number){
-        return null;
+    int fibonacciNumberFinder(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n must be a positive integer.");
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int a = 1, b = 1;
+        for (int i = 3; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
     }
 
     /*
-    Create a completely unique String identifier for a given string
-    Each createdID must contain the string n in its unaltered Form
-    if String n = "Jason"
-    then the created ID could be = Jasonklfgn3jknnvksdfm - Because it contains the unaltered String n and is unique
-
-    if you run this function twice with the same String input, it must return 2 unique String IDs
+     Returns binary value of the given int number
      */
-    String createUniqueID(String n){
-        return null;
+    String intToBinaryNumber(int number) {
+        return Integer.toBinaryString(number);
     }
 
-
+    /*
+     Create a completely unique String identifier for a given string
+     */
+    String createUniqueID(String n) {
+        if (n == null || n.isEmpty()) {
+            throw new IllegalArgumentException("Input string cannot be null or empty.");
+        }
+        String uniqueID;
+        do {
+            uniqueID = n + UUID.randomUUID().toString(); // Generate a unique ID with the input string
+        } while (uniqueIDs.contains(uniqueID));
+        uniqueIDs.add(uniqueID);
+        return uniqueID;
+    }
 }
